046b50116abbe1e61058ba8e35f1c92efafec26a
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-05-04 16:42:32 -0400

    Setup addCtrl

--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -7,8 +7,34 @@ addctrl.controller('addCtrl', function($scope, $htpp, geolocation) {
   var lat = 0;
   var long = 0;
 
-  // Set inital coordinates to the Toronto 
+  // Set inital coordinates to the Toronto
   $scope.formData.latitude = 43.6532;
   $scope.formData.longitude = -79.3832;
 
+  var
+
+  // Grabs all of the text box fields
+  $scope.createUser = function() {
+    var userData = {
+      username: $scope.formData.username,
+      gender: $scope.formData.gender,
+      age: $scope.formData.age,
+      favlang: $scope.formData.favlang,
+      location: [$scope.formData.longitude, $scope.formData.latitude],
+      htmlverified: $scope.formData.htmlverified
+    };
+
+    // Saves the user data to the db
+    $http.post('/users', userData)
+         .success(function (data) {
+          // once the post request is complete and the user is created we want to clear the form
+           $scope.formData.username = "";
+           $scope.formData.gender = "";
+           $scope.formData.age = "";
+           $scope.formData.favlang = "";
+         })
+         .error(function (data) {
+           console.log('Error: ' + data);
+         });
+  }
 });
