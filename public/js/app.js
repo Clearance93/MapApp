4d9996f3ed73d181315661377902768ae355c3e6
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-05-05 12:47:50 -0400

    Add gservice to app.js and index.html. Replace placeholder image with info for google mapp

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
 // Declaring the initial angular module "GoogleMapApp"
 // The Module will grab other controllers and services
-var app = angular.module('map_app', ['addCtrl', 'geolocation']);
+var app = angular.module('map_app', ['addCtrl', 'geolocation', 'gservice']);
