76945ed290b62c874013180b2a7caca34c6658db
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-05-05 11:34:16 -0400

    Update index.html to have the correct ng-app naem (needs to match bower app name

--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -1,6 +1,6 @@
 //  Creates the addCtrl and Controller - it has one dependency on 'gelocation'
 var addCtrl = angular.module('addCtrl', ['geolocation']);
-addctrl.controller('addCtrl', function($scope, $htpp, geolocation) {
+addCtrl.controller('addCtrl', function($scope, $http, geolocation) {
   // Intialize Variables
   $scope.formData = {};
   var coords = {};
@@ -11,10 +11,11 @@ addctrl.controller('addCtrl', function($scope, $htpp, geolocation) {
   $scope.formData.latitude = 43.6532;
   $scope.formData.longitude = -79.3832;
 
-  var
 
-  // Grabs all of the text box fields
+  // FUNCTIONS =================================================================
+  // Creates a new user based on the form
   $scope.createUser = function() {
+    // Grabs all of the text box fields
     var userData = {
       username: $scope.formData.username,
       gender: $scope.formData.gender,
@@ -26,16 +27,15 @@ addctrl.controller('addCtrl', function($scope, $htpp, geolocation) {
 
     // Saves the user data to the db
     $http.post('/users', userData)
-         .success(function (data) {
-              console.log("something worked)";
+         .success(function(data) {
           // once the post request is complete and the user is created we want to clear the form
            $scope.formData.username = "";
            $scope.formData.gender = "";
            $scope.formData.age = "";
            $scope.formData.favlang = "";
          })
-         .error(function (data) {
+         .error(function(data) {
            console.log('Error: ' + data);
          });
-  }
+  };
 });
