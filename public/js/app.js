528550f67874daaf7d81756b88c53cc7bcb5f2bb
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-06-02 15:18:10 -0400

    Refactor index to include add and query forms and added the angular routes to assist this. NOTE: there is current a werid bug in the URL /join wants /#/join and find wants /#/find

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
 // Declaring the initial angular module "GoogleMapApp"
 // The Module will grab other controllers and services
-var app = angular.module('map_app', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('map_app', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+
+  //Configures Angular routing -- showing the relevant view and controller when needed
+  .config(function($routeProvider){
+    //Join Team Control Panel
+    $routeProvider.when('/join', {
+      controller: 'addCtrl',
+      templateUrl: 'partials/addForm.html',
+      //Find teammates control panel
+    }).when('/find', {
+      templateUrl: 'partials/queryForm.html',
+
+      //All else forward to the Joing Team Control Panel
+    }).otherwise({redirectTo:'/join'})
+  });
